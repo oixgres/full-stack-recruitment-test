ee8e5d0579692accacd242e0d72969ddf98d2e53
Sergio Enrique Guerra Cervantes <60751647+oixgres@users.noreply.github.com>
2021-10-15 21:22:23 -0700

    Updates

---
+++ precache-manifest.7ce5ba7a9d66dda86ceb6859a904ba97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33f41ee9a9f980581ce",
+    "url": "/full-stack-recruitment-test/static/css/main.12feefee.css"
+  },
+  {
+    "revision": "f33f41ee9a9f980581ce",
+    "url": "/full-stack-recruitment-test/static/js/main.f33f41ee.js"
+  },
+  {
+    "revision": "209963bd78afcf47a00b4b4ee71cebf3",
+    "url": "/full-stack-recruitment-test/static/media/logo.209963bd.svg"
+  },
+  {
+    "revision": "9655913de58c32281f37328fed765669",
+    "url": "/full-stack-recruitment-test/index.html"
+  },
+  {
+    "revision": "9c47955aef0fc42d357ffb2727342d03",
+    "url": "/full-stack-recruitment-test/css.html"
+  },
+  {
+    "revision": "30764ca43ca1be9a08f8965f48a2da06",
+    "url": "/full-stack-recruitment-test/js.html"
+  }
+];
